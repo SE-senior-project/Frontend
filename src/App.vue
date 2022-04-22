52e86ff922159c02e5dc6bfaf4b416b7294c7ec9
PasakonPJ <paskon_paingjai@cmu.ac.th>
2022-04-22 12:04:08 +0700

    Navbar

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,13 @@
     <router-link to="/about">About</router-link>
     #F25A38
   </nav> -->
-
-  <nav class="navbar navbar-expand-lg">
+  <OnemeasureNavigation />
+  <br />
+  <br />
+  <br />
+  <br />
+  <br />
+  <!-- <nav class="navbar navbar-expand-lg">
     <div class="d-flex justify-content-start">
       <h3><b>OneMeasure</b></h3>
       <img src="../src/assets/logonav.jpg" alt="" width="100" height="60" />
@@ -15,9 +20,17 @@
         <b>Sign Out</b>
       </button>
     </div>
-  </nav>
+  </nav> -->
   <router-view />
 </template>
+<script>
+import OnemeasureNavigation from "./components/OnemeasureNavigation.vue";
+export default {
+  components: {
+    OnemeasureNavigation,
+  },
+};
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -26,11 +39,11 @@
   text-align: center;
   color: #212326de;
 }
-nav {
+/* nav {
   box-shadow: 0 5px 8px 0 rgba(0, 0, 0, 0.2), 0 9px 26px 0 rgba(0, 0, 0, 0.19);
   background-color: #ff914d;
-}
-.sameline {
+} */
+/* .sameline {
   padding-left: 20px;
   color: white;
 }
@@ -45,7 +58,7 @@ nav {
   box-shadow: 0 5px 8px 0 rgba(245, 103, 1, 0.2),
     0 9px 26px 0 rgba(245, 103, 1, 0.19);
   font-style: italic;
-}
+} */
 /* nav {
   padding: 30px;
 } */
