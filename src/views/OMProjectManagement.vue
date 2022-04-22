8088e90ff6e657d4636bb2121b966b66f75e5b64
PasakonPJ <paskon_paingjai@cmu.ac.th>
2022-04-22 22:59:01 +0700

    add create new project button

--- src/views/OMProjectManagement.vue
+++ src/views/OMProjectManagement.vue
@@ -1,11 +1,21 @@
 <template>
-  <div>
+  <br />
+
+  <div class="wrapper">
+    <ul class="my-row">
+      <li><div class="OnemeasurePM">Project Onemeasure</div></li>
+      <li></li>
+      <li>
+        <div class="buttonPM">
+          <button type="submit" class="btn btn-light btnSubmit">
+            <b>Create New Project</b>
+          </button>
+        </div>
+      </li>
+    </ul>
     <br />
-    <div class="wrapper">
-      <div class="OnemeasurePM">Project Onemeasure</div>
-      <div class="row">
-        <OnemeasurePM v-for="x in users" :key="x.id" :user="x" />
-      </div>
+    <div class="row">
+      <OnemeasurePM v-for="x in users" :key="x.id" :user="x" />
     </div>
   </div>
 </template>
@@ -46,6 +56,36 @@ export default {
 };
 </script>
 <style scoped>
+li {
+  text-decoration: none;
+  position: relative;
+}
+ul.my-row {
+  display: table;
+  width: 100%;
+  text-align: center;
+  margin: 0 auto;
+  position: relative;
+}
+
+ul.my-row > li {
+  display: table-cell;
+}
+.buttonPM {
+  text-align: right;
+  padding-right: 50px;
+}
+.btnSubmit {
+  color: #282726;
+  background-color: #fff;
+  border: 1px solid black;
+  padding: 15px;
+  border-radius: 10px;
+}
+.btnSubmit:hover {
+  box-shadow: 0 5px 8px 0 rgba(245, 103, 1, 0.2),
+    0 9px 26px 0 rgba(245, 103, 1, 0.19);
+}
 @import url("https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;400;900&display=swap");
 * {
   margin: 0;
@@ -55,17 +95,13 @@ export default {
 }
 
 .OnemeasurePM {
-  width: 95%;
   margin: 0 auto;
   height: 50px;
-  text-align: left;
-  display: flex;
-  justify-content: left;
-  align-items: left;
   font-size: 20px;
   text-transform: uppercase;
-  letter-spacing: 5px;
   font-weight: 900;
+  text-align: left;
+  padding-left: 50px;
 }
 @media screen and (max-width: 800px) {
   .OnemeasurePM {
