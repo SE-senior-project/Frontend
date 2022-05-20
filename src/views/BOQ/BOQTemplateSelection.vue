243b9c58eebc575c9e8cbbbdd92bc279e0f42c2d
Domdae <domdae@Phonmongkons-MacBook-Pro.local>
2022-05-20 15:15:09 +0700

    [feature]: boq template selection page's flow

---
+++ src/views/BOQ/BOQTemplateSelection.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- list -->
+  <div class="container"></div>
+  <!-- BOQ Table -->
+  <div class="container">
+    <button class="btn btn-primary">
+      <router-link :to="{ name: 'boq_template' }"> Back </router-link>
+    </button>
+    <button class="btn btn-primary">
+      <router-link :to="{ name: 'boq_generation' }">
+        Use as template
+      </router-link>
+    </button>
+  </div>
+  <!-- Price Tread pf material -->
+  <div class="container"></div>
+</template>
+<script>
+export default {
+  name: "boq_confirmation",
+};
+</script>
+
+<style scoped></style>
