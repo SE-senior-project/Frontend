2bdd837a1068f370600eab6eb1b1cb208eaca232
forio2 <forio2@hotmail.com>
2022-06-06 08:56:45 +0700

    [feature]: admin

--- src/views/admin/ManageAccount.vue
+++ src/views/admin/ManageAccount.vue
@@ -1,21 +1,32 @@
 <template>
-  <div class="contractor">
-    <a :href="'admin'">Approval Account</a>
-    | <a :href="'manage_account'">Manage Account</a>
-  </div>
-  <div>
-    <h5>Contractors</h5>
-    <div class="contractor">
-      <ContactorCard v-for="x in users" :key="x.id" :user="x" />
+  <div class="relative flex justify-center items-center">
+    <div class="max-w-5xl bg-white shadow-xl rounded-lg w-[1028px] mb-4 mx-8 px-10 py-5">
+      <div class="header float-left">
+        <a :href="'admin'">การอนุมัติบัญชี</a>
+        | <a :href="'manage_account'">การจัดการบัญชี</a>
+      </div>
+      <div class="clear-both">
+        <div>
+          <TextLabel class="text-2xl mt-[50px] mb-10" label="ผู้รับเหมา" />
+          <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+            <ContactorCard label="เปิดการใช้งาน" v-for="x in users" :key="x.id" :user="x" />
+            
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 <script>
 import ContactorCard from "../../components/admin/ContactorCard.vue";
+import PrimaryButton from "@/components/button/PrimaryButton";
+import TextLabel from "@/components/field/TextLabel";
 export default {
   name: "manage_account",
   components: {
     ContactorCard,
+    PrimaryButton,
+    TextLabel,
   },
   data() {
     return {
