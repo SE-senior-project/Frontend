d571e37f47370633e08e92c436104f018eb24107
Domdae <domdae@Phonmongkons-MacBook-Pro.local>
2022-06-06 16:15:22 +0700

    [feature]: project page

--- src/components/pm/ProjectCard.vue
+++ src/components/pm/ProjectCard.vue
@@ -1,24 +1,29 @@
 <template>
-  <div class="col-3">
+  <div class="mx-[60px] lg:mx-[40px] mb-10 shadow-xl rounded-lg">
     <router-link
-      class="link"
+      class="link "
       :to="{ name: 'material_list', params: { id: user.id } }"
     >
-      <div class="cards_wrap">
-        <div class="card_item">
-          <div class="card_inner">
-            <div class="role_name">
-              <b>Project {{ user.id }}</b>
-            </div>
-            <div class="real_name">
-              <b>{{ user.name }} </b>
-            </div>
-            <div class="film">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </div>
-          </div>
-        </div>
+      <div
+        class="
+          contractor
+          text-white
+          relative
+          mb-[20px]
+          text-lg text-center
+          py-[50px]
+          bg-orange-500
+        "
+      >
+        <span>โปรเจคที่ {{ user.id }}</span>
+      </div>
+      <div class="text-sm font-bold w-5/6 m-auto mb-[20px]">
+        <span>ชื่อ: {{ user.name }}</span>
+      </div>
+      <hr>
+      <div class="text-sm px-[20px] pb-[50px]">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
       </div>
     </router-link>
   </div>
@@ -36,91 +41,12 @@ export default {
 </script>
 
 <style scoped>
-.cards_wrap {
-  padding: 20px;
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  flex-wrap: wrap;
-}
-
-.cards_wrap .card_item {
-  width: 100%;
-  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
-}
-
-.cards_wrap .card_inner {
-  background: #fff;
-  border-radius: 5px;
-  border: 1px solid black;
-  min-width: 225px;
-  min-height: 315px;
-  max-height: 370px;
-  width: 100%;
-}
-
-.cards_wrap .card_item .role_name {
-  padding-top: 20px;
-  color: #ea971a;
-  font-weight: 900;
-  letter-spacing: 2px;
-  text-transform: uppercase;
-  /* font-size: 20px; */
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-}
-
-.cards_wrap .card_item .real_name {
-  color: #745042;
-  /* font-size: 12px; */
-  font-weight: 100;
-  margin: 5px 0 10px;
-}
-
-.cards_wrap .card_item .film {
-  font-size: 14px;
-  line-height: 24px;
-  color: #7b8ca0;
-}
-
-@media screen and (max-width: 1024px) {
-  .cards_wrap .card_item {
-    width: 33%;
-  }
-}
-
-@media screen and (max-width: 768px) {
-  .cards_wrap .card_item {
-    width: 50%;
-  }
-  .wrapper .header {
-    font-size: 16px;
-    height: 60px;
-  }
-}
-
-@media screen and (max-width: 568px) {
-  .cards_wrap .card_item {
-    width: 100%;
-  }
-  .wrapper .header {
-    font-size: 14px;
-  }
-}
-/* .card {
-  border: 1px solid black;
-  padding: 30px;
-  margin: 10px 42px;
-  cursor: grab;
-} */
-
-.cards_wrap :hover {
-  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
-}
-
-.link {
-  color: #2c3e50;
-  text-decoration: none;
+hr {
+    display: block;
+    height: 1px;
+    border: 0;
+    border-top: 1px solid #edeff2;
+    margin: 1em 0;
+    padding: 0;
 }
 </style>
--- src/views/pm/Project.vue
+++ src/views/pm/Project.vue
@@ -1,35 +1,46 @@
 <template>
-  <br />
-
-  <div class="wrapper">
-    <ul class="my-row">
-      <li><div class="OnemeasurePM">Project Onemeasure</div></li>
-      <li></li>
-      <li>
-        <div class="buttonPM">
-          <button type="submit" class="btn btn-light btnSubmit">
-            <router-link id="createnewbutt" :to="{ name: 'create_project' }">
-              <b>Create New Project</b></router-link
-            >
-          </button>
-        </div>
-      </li>
-    </ul>
-    <br />
-    <ul class="my-row">
-      <li>
+  <div class="relative flex justify-center items-center">
+    <div
+      class="
+        max-w-5xl
+        bg-white
+        shadow-xl
+        rounded-lg
+        w-[1028px]
+        mb-4
+        mx-8
+        px-10
+        py-5
+      "
+    >
+      <FormWrapper label="โปรเจค" />
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         <ProjectCard v-for="x in users" :key="x.id" :user="x" />
-      </li>
-    </ul>
+      </div>
+      <router-link :to="{ name: 'create_project' }">
+        <PrimaryButton class="float-right mb-[20px]">สร้างโปรเจคใหม่</PrimaryButton>
+      </router-link>
+      <FormWrapper class="clear-both flex flex-row" label="โปรเจคที่ซ่อนไว้">
+        <img @click='toggle = !toggle' v-if="!toggle" class="h-[25px] pl-[15px] p-[5px]" src="../../assets/right-arrow.png" />
+        <img @click='toggle = !toggle' v-if='toggle' class="h-[27px] pl-[15px] p-[5px]" src="../../assets/down-arrow.png" />
+      </FormWrapper>
+      <div v-if='toggle' class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        <ProjectCard v-for="x in users" :key="x.id" :user="x" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import ProjectCard from "../../components/pm/ProjectCard.vue";
+import FormWrapper from "@/components/form/FormWrapper";
+import PrimaryButton from "@/components/button/PrimaryButton";
 export default {
   name: "OMprojectmanagement",
   components: {
     ProjectCard,
+    FormWrapper,
+    PrimaryButton
   },
   data() {
     return {
@@ -55,6 +66,7 @@ export default {
           id: 5,
         },
       ],
+      toggle: false,
     };
   },
 };
